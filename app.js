ceda77d0415bb6fbbabcc734ac5dc455a69a804b
Alexis Dubria Desiatco <99580667+Alexx009@users.noreply.github.com>
2023-11-22 15:18:01 +0800

    TEST

--- app.js
+++ app.js
@@ -1,28 +1,6 @@
 // Array to store all PDF file names
 let allPDFFiles = [];
 
-// Function to fetch PDF files from the server and store them in an array
-async function fetchPDFList() {
-  try {
-    const response = await fetch('/3GS-INSURANCE/INSURANCE/G2/');
-
-
-    const data = await response.text();
-
-    // Parse the HTML response to extract PDF file names
-    const parser = new DOMParser();
-    const htmlDoc = parser.parseFromString(data, 'text/html');
-    const links = Array.from(htmlDoc.querySelectorAll('a[href$=".pdf"]'));
-
-    // Extract file names and create links
-    allPDFFiles = links.map(link => link.textContent.split('.pdf')[0].trim());
-
-    createPDFLinks(allPDFFiles);
-  } catch (error) {
-    console.error('Error fetching PDF list:', error);
-  }
-}
-
 // Function to create links for each PDF file
 function createPDFLinks(pdfFiles) {
   const pdfList = document.getElementById('pdfList');
@@ -33,13 +11,23 @@ function createPDFLinks(pdfFiles) {
     const pdfLink = document.createElement('a');
 
     // Adjust the path based on your repository structure
-    pdfLink.href = `/3GS-INSURANCE/INSURANCE/G2/${fileName}.pdf`;
-
+    pdfLink.href = `${fileName}.pdf`;
 
     pdfLink.textContent = fileName;
     listItem.appendChild(pdfLink);
     pdfList.appendChild(listItem);
   });
+}
+
+// Function to read the JSON file and populate the allPDFFiles array
+function fetchPDFList() {
+  fetch('G1.json') // replace with the actual path to your JSON file
+    .then(response => response.json())
+    .then(data => {
+      allPDFFiles = data.pdf_files; // assuming the JSON structure has a key 'pdf_files'
+      createPDFLinks(allPDFFiles);
+    })
+    .catch(error => console.error('Error fetching JSON file:', error));
 }
 
 // Function to filter PDF list based on user input
---
+++ test.py
@@ -0,0 +1,20 @@
+import os
+import json
+
+# Specify the directory containing the PDF files
+pdf_directory = './INSURANCE/G1'
+
+# Get a list of all PDF files in the directory
+pdf_files = [f for f in os.listdir(pdf_directory) if f.endswith('.pdf')]
+
+# Create a dictionary to store the file names
+pdf_dict = {'pdf_files': pdf_files}
+
+# Specify the path for the JSON file
+json_file_path = 'G1.json'
+
+# Write the dictionary to a JSON file
+with open(json_file_path, 'w') as json_file:
+    json.dump(pdf_dict, json_file)
+
+print(f'JSON file created at {json_file_path}')
